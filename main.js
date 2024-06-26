43133b1189895b5336f39b43a5759d850ac710c1
PatricioDVG <140539670+PatricioDVG@users.noreply.github.com>
2024-06-26 17:29:53 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
 function setup(){
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -7,7 +10,10 @@ function setup(){
 poseNet.on('pose', gotPoses)
 }
 function draw(){
-    background ('#279A43')
+    background ('#279A43');
+    textSize(difference);
+    fill(#123456);
+    text('hola, si se portan bien hoy, vamos a jugar mañana', 10, 20)
 }
 function modelLoaded(){
     console.log('PoseNet se inicializó');
@@ -17,5 +23,9 @@ function gotPoses(results)
     if(results.length > 0)
         {
             console.log(results);
+             leftWristX = results[0].pose.leftWrist.x;
+            righttWristX = results[0].pose.rightWrist.x;
+            difference = floor(leftWristX - rightWristX);
+
         }
 }
